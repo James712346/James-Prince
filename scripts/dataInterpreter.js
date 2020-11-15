ddc8407cbf4d3fda62c4edf5d914c234143e4fec
James Prince <james712346@gmail.com>
2020-11-16 09:26:22 +1000

    Parsed the Json

--- scripts/dataInterpreter.js
+++ scripts/dataInterpreter.js
@@ -1,3 +1,3 @@
 //fetch('/scripts/data.json').then(response => response.text()).then(text => console.log(text))
-var data = fetch('/scripts/data.json')
+var data = JSON.parse(fetch('/scripts/data.json'))
 console.log(data)
