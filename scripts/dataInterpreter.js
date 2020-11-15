7f6afa3805f6d707ebc9d7e0ece77b67c86aede5
James Prince <james712346@gmail.com>
2020-11-16 09:23:11 +1000

    Testing json

--- scripts/dataInterpreter.js
+++ scripts/dataInterpreter.js
@@ -1,1 +1,3 @@
-fetch('/scripts/data.json').then(response => response.text()).then(text => console.log(text))
+//fetch('/scripts/data.json').then(response => response.text()).then(text => console.log(text))
+var data = fetch('/scripts/data.json')
+console.log(data)
