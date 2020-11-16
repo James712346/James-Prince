b2be9ea32762ea85fd89fc5707e53ab8db581d7c
James Prince <james712346@gmail.com>
2020-11-16 10:41:43 +1000

    test2

--- scripts/dataInterpreter.js
+++ scripts/dataInterpreter.js
@@ -1,3 +1,3 @@
 //fetch('/scripts/data.json').then(response => response.text()).then(text => console.log(text))
-var data = fetch('/scripts/data.json').json();
+var data = fetch('/scripts/data.json').then(repsonse => response.json()).then(rep);
 console.log(data);
